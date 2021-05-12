84092d4491090abe7f620080efab6c478205a5ce
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-12 11:50:43 -0600

    Add comments migration

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: %i[show update]
+
+  def index
+    render json: Post.all
+  end
+
+  def show
+    if @post
+      render json: @post
+    else
+      render json: { message: 'Post could not be found.' }
+    end
+  end
+
+  def update; end
+
+  private
+
+  def get_post
+    @post ||= Post.find_by(id: params[:id])
+  end
+
 end
