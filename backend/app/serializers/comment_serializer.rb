7d4661a17a0441db61bc563112393b26122c3a70
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-15 05:55:34 -0600

    Clean up serializers

--- backend/app/serializers/comment_serializer.rb
+++ backend/app/serializers/comment_serializer.rb
@@ -1,3 +1,4 @@
 class CommentSerializer < ApplicationSerializer
   attributes :id, :name, :content, :created_at
+  belongs_to :post
 end
--- backend/app/serializers/post_serializer.rb
+++ backend/app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer < ApplicationSerializer
-  attributes :id, :name, :content, :created_at
+  attributes :id, :name, :content, :likes, :created_at
   has_many :comments
 end
