df292ea7d8e20d54f750fbb04d9a19f157301147
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-20 16:35:56 -0400

    Refactor classes out
    - Doesn't work because js classes are used outside of module.

---
+++ frontend/src/javascript/comment.js
@@ -0,0 +1,23 @@
+import Content from './content'
+
+class Comment extends Content {
+    constructor(id, name, content, createdAt) {
+        super(id, name, content, createdAt);
+    }
+
+    toListItem() {
+        let li = document.createElement('li')
+        li.classList.add('comment')
+
+        let h4 = document.createElement('h4')
+        h4.innerText = this.name + " " + this.createdAt()
+
+        let p = document.createElement('p')
+        p.innerText = this.content
+
+        li.append(h4, p)
+        return li
+    }
+}
+
+export default Comment
---
+++ frontend/src/javascript/content.js
@@ -0,0 +1,15 @@
+class Content {
+    constructor(id, name, content, createdAt) {
+        this.id = id
+        this.name = name
+        this.content = content
+        this._createdAt = createdAt
+    }
+
+    createdAt() {
+        let date = this._createdAt.substring(0, 10).split('-')
+        return `( ${date[1]}/${date[2]}/${date[0]} )`
+    }
+}
+
+export default Content
